f5334e9b232ad50b230dde865d0ea74ca0df64c6
Michał Wyszomirski <bimgissql@users.noreply.github.com>
2017-12-28 16:57:41 +0100

    Add files via upload

---
+++ Oracle/execute.py
@@ -0,0 +1,46 @@
+import cx_Oracle
+import yaml
+
+# Get database connection information from YML file
+with open("oracle.yml", 'r') as stream:
+    config = yaml.load(stream)
+
+# Parse connection data	
+host = config[':host'] 
+port = config[':port']
+sid = config[':sid']
+user = config[':user']
+password = config[':password']
+
+# Show information
+print('Connect to Oracle database')
+print('Oracle server   :', host)
+print('Oracle port     :', port)
+print('Oracle SID      :', sid)
+
+# Prepare DSN
+dsnStr = cx_Oracle.makedsn(host, port, sid)
+#print('Oracle DSN      :', dsnStr)
+
+# Connect to the database
+con = cx_Oracle.connect(user = user, password = password, dsn = dsnStr)
+
+# Get Oracle version 
+print('Oracle version  :', con.version)
+
+# Open cursor
+cursor = con.cursor()
+
+# Execute SQL statement
+cursor.execute("SELECT * FROM HELP WHERE TOPIC = 'CONNECT'")
+#cursor.fetchall()
+
+# Print query results
+for row in cursor.fetchall():
+    print(row)
+
+# Close cursor
+cursor.close()
+
+# Close connection
+con.close()
